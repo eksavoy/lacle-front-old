1b87fb34be9199682cf62a2d84f1f4a68edad451
Romain de-coster <romain@pop.eu.com>
2017-09-14 11:43:08 +0200

    remove autoUpdate for mac

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { app, BrowserWindow, autoUpdater } from 'electron';
-import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
-import { enableLiveReload } from 'electron-compile';
+import {app, BrowserWindow, autoUpdater} from 'electron';
+import installExtension, {REACT_DEVELOPER_TOOLS} from 'electron-devtools-installer';
+import {enableLiveReload} from 'electron-compile';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -10,39 +10,61 @@ const isDevMode = process.execPath.match(/[\\/]electron/);
 const server = 'http://electron-server.poptech-devs.ovh';
 const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
 
-//autoUpdater.setFeedURL(feed);
+if (process.platform !== "darwin") {
+    autoUpdater.setFeedURL(feed);
 
-// setInterval(() => {
-//   autoUpdater.checkForUpdates();
-// }, 60000);
+    setInterval(() => {
+        autoUpdater.checkForUpdates();
+    }, 60000);
+
+    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+        const dialogOpts = {
+            type: 'info',
+            buttons: ['Restart'],
+            title: 'Mise à jour installé veuillez redémarer',
+            message: `La version ${app.getVersion()} est installée`,
+            detail: 'Une mise à jour est installé pour utiliser cette mise à jour vous devez redémarrer l\'application'
+        }
+
+        dialog.showMessageBox(dialogOpts, (response) => {
+            if (response === 0) autoUpdater.quitAndInstall();
+        })
+    });
+
+    autoUpdater.on('error', message => {
+        console.error('There was a problem updating the application');
+        console.error(message);
+    })
+}
+
 
 if (isDevMode) enableLiveReload({strategy: 'react-hmr'});
 
 const createWindow = async () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
-  
-  mainWindow.maximize();
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.maximize();
 
-  // Open the DevTools.
-  if (isDevMode) {
-    await installExtension(REACT_DEVELOPER_TOOLS);
-    mainWindow.webContents.openDevTools();
-  }
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Open the DevTools.
+    if (isDevMode) {
+        await installExtension(REACT_DEVELOPER_TOOLS);
+        mainWindow.webContents.openDevTools();
+    }
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 };
 
 // This method will be called when Electron has finished
@@ -52,41 +74,23 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 app.commandLine.appendSwitch('--enable-touch-events');
 
-// autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-//     const dialogOpts = {
-//         type: 'info',
-//         buttons: ['Restart'],
-//         title: 'Mise à jour installé veuillez redémarer',
-//         message: `La version ${app.getVersion()} est installée`,
-//         detail: 'Une mise à jour est installé pour utiliser cette mise à jour vous devez redémarrer l\'application'
-//     }
-//
-//     dialog.showMessageBox(dialogOpts, (response) => {
-//         if (response === 0) autoUpdater.quitAndInstall();
-//     })
-// });
-//
-// autoUpdater.on('error', message => {
-//     console.error('There was a problem updating the application');
-//     console.error(message);
-// })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
